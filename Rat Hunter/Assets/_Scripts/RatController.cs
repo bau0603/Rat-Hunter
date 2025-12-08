df7344d9a8ed1cc9c8a6fea2c6758e804686f187
bau0603 <bau@siue.edu>
2025-12-07 22:06:26 -0600

    Merged with main
    Included Kehajana's and Brandon's work

--- Rat Hunter/Assets/_Scripts/RatController.cs
+++ Rat Hunter/Assets/_Scripts/RatController.cs
@@ -238,35 +238,24 @@ public class RatController : MonoBehaviour
     }
 
     // Backwards compatibility with existing Projectile script
-   void OnTriggerEnter(Collider other)
-{
-    if (isCaptured || currentState == RatState.Captured) return;
-
-    Projectile projectile = other.GetComponent<Projectile>();
-    if (projectile != null)
+    void OnTriggerEnter(Collider other)
     {
-        if (projectile.type == Projectile.ProjectileType.Tranquilizer)
+        if (isCaptured || currentState == RatState.Captured) return;
+
+        Projectile projectile = other.GetComponent<Projectile>();
+        if (projectile != null)
         {
-            
-            if (currentState == RatState.Normal)
+            if (projectile.type == Projectile.ProjectileType.Tranquilizer)
             {
                 GetTranquilized();
             }
-            
-            else if (currentState == RatState.Tranquilized)
+            else if (projectile.type == Projectile.ProjectileType.Net && isTranquilized)
             {
-                GetCaptured();   
+                GetCaptured();
             }
-        }
-        else if (projectile.type == Projectile.ProjectileType.Net)
-        {
-            
-            GetCaptured();
-        }
 
-        
-        Destroy(other.gameObject);
+            // Destroy the projectile
+            Destroy(other.gameObject);
+        }
     }
-}
-
 }
