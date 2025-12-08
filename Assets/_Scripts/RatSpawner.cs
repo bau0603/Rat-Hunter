3c5b3e1b17e38f21a2f25972daee7dbcda734a2b
Syeda Zunehra Banu <118747783+zunehraBanu@users.noreply.github.com>
2025-12-07 21:57:39 -0600

    Add Level 3 City Street level, rats, obstacles, visuals

---
+++ Assets/_Scripts/RatSpawner.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RatSpawner : MonoBehaviour
+{
+    [Header("Spawning Settings")]
+    public List<GameObject> ratPrefabs;
+    public float minSpawnTime = 1f;
+    public float maxSpawnTime = 3f;
+    public int maxRats = 10;
+
+    [Range(0f, 1f)]
+    public float speedyRatChance = 0.2f; // 20% chance for the Speedy Rat
+
+    [Range(0f, 1f)]
+public float tankRatChance = 0.1f;   // NEW – maybe 10% to start
+
+    private int currentRats = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnRats());
+    }
+
+    IEnumerator SpawnRats()
+    {
+        while (true)
+        {
+            if (RatHunter.Instance != null && RatHunter.Instance.isGameActive && currentRats < maxRats)
+            {
+                yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+
+                if (RatHunter.Instance.isGameActive && currentRats < maxRats)
+                {
+                    SpawnRat();
+                }
+            }
+            yield return null;
+        }
+    }
+
+    void SpawnRat()
+{
+    if (ratPrefabs == null || ratPrefabs.Count == 0)
+    {
+        Debug.LogError("Rat prefab is not assigned in RatSpawner!");
+        return;
+    }
+
+    GameObject selectedRatPrefab = ratPrefabs[0]; // default to Base
+
+    float roll = Random.value;
+
+    // If we have 3 prefabs: 0 = Base, 1 = Speedy, 2 = Tank
+    if (ratPrefabs.Count >= 3)
+    {
+        if (roll < tankRatChance)
+        {
+            selectedRatPrefab = ratPrefabs[2]; // Tank
+        }
+        else if (roll < tankRatChance + speedyRatChance)
+        {
+            selectedRatPrefab = ratPrefabs[1]; // Speedy
+        }
+        else
+        {
+            selectedRatPrefab = ratPrefabs[0]; // Base
+        }
+    }
+    else if (ratPrefabs.Count >= 2)
+    {
+        // Old behaviour: just Base + Speedy
+        if (roll < speedyRatChance)
+        {
+            selectedRatPrefab = ratPrefabs[1]; // Speedy
+        }
+        else
+        {
+            selectedRatPrefab = ratPrefabs[0]; // Base
+        }
+    }
+
+    GameObject rat = Instantiate(selectedRatPrefab, transform.position, Quaternion.identity);
+    currentRats++;
+
+    RatController ratController = rat.GetComponent<RatController>();
+    if (ratController != null)
+    {
+        RatTracker tracker = rat.AddComponent<RatTracker>();
+        tracker.OnRatDestroyed += () => currentRats--;
+    }
+    else
+    {
+        Debug.LogError("RatController component missing on spawned rat prefab!");
+    }
+}
+}
+// Helper component to track rat destruction
+public class RatTracker : MonoBehaviour
+{
+    public System.Action OnRatDestroyed;
+
+    void OnDestroy()
+    {
+        OnRatDestroyed?.Invoke();
+    }
+}
--- README.md
+++ README.md
@@ -1,29 +1,22 @@
-CS382 Final Game Project
-Due Date: 12/8/25
-Authors:
+CS382 Final
+By Brandon Au, Syeda Zunehra Banu, Sandra Lovino, Kehajana Tallie
 
-Rat Hunters
+Alpha Version Finished: 11/16/2025
 
-Game Description:
+# Rat Hunter
 
-Instructions:
+Description: 
+You are an animal control officer in charge of preventing rats from infesting streets
+and homes. Armed with a tranquilizer and net gun, your job is to tranquilize then capture
+rodents. From the countryside to the busy streets of the city, stop the rats from escaping and
+invading the area. 
 
-Features:
-
-
-
-
-Credits here:
-
-Images used:
-
-The Rats
-
-from https://www.picmix.com/stamp/jerma985-jerma-rat-2131135
-
-Audio Clips used:
-
-FX061.mp3
-FX065.mp3
-
-from https://opengameart.org/content/sound-effects-sfx007
+Current Objectives:
+Aim and alternate shooting between a tranquilizer and a net gun with the left and right
+mouse clicks respectively. 
+Rats will run around from left to right and vice versa to evade
+captures. 
+Other objects will be placed in the game to trick players, and if a player hits it, they
+lose points. 
+The level ends when all rats are captured, the player hits too many objects, or the time
+runs out. 
